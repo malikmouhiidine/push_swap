aecbdd9b5ff12d4e457b5fc7ab5a867882de82fa
Malik Mouhiidine <malikprofessional2020@gmail.com>
2024-01-20 00:50:31 +0100

    Solved norm errors in bonus

--- bonus/args_handler.c
+++ bonus/args_handler.c
@@ -6,40 +6,45 @@
 /*   By: mmouhiid <mmouhiid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 22:25:13 by mmouhiid          #+#    #+#             */
-/*   Updated: 2024/01/19 18:49:45 by mmouhiid         ###   ########.fr       */
+/*   Updated: 2024/01/20 00:46:30 by mmouhiid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-void	iterate_through_args(int argc, char **argv, t_list **stack_a)
+void	process_arg(char *arg, t_list **stack_a)
 {
 	int		i;
 	int		*num;
 	char	**tmp_nums;
 
+	tmp_nums = ft_split(arg, ' ');
+	if (!tmp_nums || !*tmp_nums)
+		exit_handler(1);
+	i = 0;
+	while (tmp_nums[i])
+		i++;
+	while (i--)
+	{
+		if (is_valid_int(tmp_nums[i]))
+		{
+			num = (int *)malloc(sizeof(int));
+			*num = ft_atoi(tmp_nums[i]);
+			ft_lstadd_front(stack_a, ft_lstnew(num));
+		}
+		else
+			exit_handler(1);
+	}
+	i = 0;
+	while (tmp_nums[i])
+		free(tmp_nums[i++]);
+	free(tmp_nums);
+}
+
+void	iterate_through_args(int argc, char **argv, t_list **stack_a)
+{
 	while (argc-- > 1)
 	{
-		tmp_nums = ft_split(argv[argc], ' ');
-		if (!tmp_nums || !*tmp_nums)
-			exit_handler(1);
-		i = 0;
-		while (tmp_nums[i])
-			i++;
-		while (i--)
-		{
-			if (is_valid_int(tmp_nums[i]))
-			{
-				num = (int *)malloc(sizeof(int));
-				*num = ft_atoi(tmp_nums[i]);
-				ft_lstadd_front(stack_a, ft_lstnew(num));
-			}
-			else
-				exit_handler(1);
-		}
-		i = 0;
-		while (tmp_nums[i])
-			free(tmp_nums[i++]);
-		free(tmp_nums);
+		process_arg(argv[argc], stack_a);
 	}
 }
--- bonus/helpers.c
+++ bonus/helpers.c
@@ -6,7 +6,7 @@
 /*   By: mmouhiid <mmouhiid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 22:25:13 by mmouhiid          #+#    #+#             */
-/*   Updated: 2024/01/19 18:48:07 by mmouhiid         ###   ########.fr       */
+/*   Updated: 2024/01/20 00:48:43 by mmouhiid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,28 +28,31 @@ int	ft_strcmp(char *s1, char *s2)
 
 int	is_valid_int(char *str)
 {
-    int sign = 1;
-    unsigned long long num = 0;
+	int					sign;
+	unsigned long long	num;
 
-    if (*str == '-')
-    {
-        sign = -1;
-        if (!*++str || *str == '0')
-            return (0);
-    }
-    while (*str)
-    {
-        if (*str < '0' || *str > '9')
-            return (0);
-        if (num > ULLONG_MAX / 10 || (num == ULLONG_MAX / 10 && (*str - '0') > (int)(ULLONG_MAX % 10)))
-            return (0);
-        num = num * 10 + (*str++ - '0');
-        if (sign == 1 && num > INT_MAX)
-            return (0);
-        if (sign == -1 && num > (unsigned long long)INT_MAX + 1)
-            return (0);
-    }
-    return (1);
+	sign = 1;
+	num = 0;
+	if (*str == '-')
+	{
+		sign = -1;
+		if (!*++str || *str == '0')
+			return (0);
+	}
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+			return (0);
+		if (num > ULLONG_MAX / 10 || (num == ULLONG_MAX / 10
+				&& (*str - '0') > (int)(ULLONG_MAX % 10)))
+			return (0);
+		num = num * 10 + (*str++ - '0');
+		if (sign == 1 && num > INT_MAX)
+			return (0);
+		if (sign == -1 && num > (unsigned long long)INT_MAX + 1)
+			return (0);
+	}
+	return (1);
 }
 
 int	is_valid_operation(char *operation)
