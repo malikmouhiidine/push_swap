577f780066ddc5832a91782936b0cf255a26cfa1
Malik Mouhiidine <malikprofessional2020@gmail.com>
2024-01-20 17:59:56 +0100

    Revert "Solved norm errors, without testing if it still works (normal behavior, no leaks), still the test and Makefile)"
    This reverts commit 0c2389098414e8e593e9b2011312c6fea50abd13.

--- push_swap/manipulation_helpers.c
+++ push_swap/manipulation_helpers.c
@@ -6,11 +6,103 @@
 /*   By: mmouhiid <mmouhiid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 02:14:18 by mmouhiid          #+#    #+#             */
-/*   Updated: 2024/01/20 02:37:21 by mmouhiid         ###   ########.fr       */
+/*   Updated: 2024/01/19 15:52:22y mmouhiid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
+
+t_list	*push_node_target_totop(t_list **stack_b, t_list **stack_a,
+			t_list *node, t_list *target)
+{
+	t_list	*operations;
+	int		node_index;
+	int		target_index;
+	int		stack_b_size;
+	int		stack_a_size;
+
+	operations = NULL;
+	node_index = get_index(*stack_b, node);
+	target_index = get_index(*stack_a, target);
+	stack_b_size = ft_lstsize(*stack_b);
+	stack_a_size = ft_lstsize(*stack_a);
+	if (node_index < (float)stack_b_size / 2.0
+		&& target_index < (float)stack_a_size / 2.0)
+	{
+		while (node_index > 0 && target_index > 0)
+		{
+			ft_lstadd_back(&operations, ft_lstnew("rr\n"));
+			node_index--;
+			target_index--;
+		}
+		while (node_index > 0)
+		{
+			ft_lstadd_back(&operations, ft_lstnew("rb\n"));
+			node_index--;
+		}
+		while (target_index > 0)
+		{
+			ft_lstadd_back(&operations, ft_lstnew("ra\n"));
+			target_index--;
+		}
+	}
+	else if (node_index >= (float)stack_b_size / 2.0
+		&& target_index >= (float)stack_a_size / 2.0)
+	{
+		while (node_index < stack_b_size && target_index < stack_a_size)
+		{
+			ft_lstadd_back(&operations, ft_lstnew("rrr\n"));
+			node_index++;
+			target_index++;
+		}
+		while (node_index < stack_b_size)
+		{
+			ft_lstadd_back(&operations, ft_lstnew("rrb\n"));
+			node_index++;
+		}
+		while (target_index < stack_a_size)
+		{
+			ft_lstadd_back(&operations, ft_lstnew("rra\n"));
+			target_index++;
+		}
+	}
+	else
+	{
+		if (node_index < (float)stack_b_size / 2.0)
+		{
+			while (node_index > 0)
+			{
+				ft_lstadd_back(&operations, ft_lstnew("rb\n"));
+				node_index--;
+			}
+		}
+		else
+		{
+			while (node_index < stack_b_size)
+			{
+				ft_lstadd_back(&operations, ft_lstnew("rrb\n"));
+				node_index++;
+			}
+		}
+		if (target_index < (float)stack_a_size / 2.0)
+		{
+			while (target_index > 0)
+			{
+				ft_lstadd_back(&operations, ft_lstnew("ra\n"));
+				target_index--;
+			}
+		}
+		else
+		{
+			while (target_index < stack_a_size)
+			{
+				ft_lstadd_back(&operations, ft_lstnew("rra\n"));
+				target_index++;
+			}
+		}
+	}
+	return (operations);
+}
 
 t_list	*make_min_on_top_ops(t_list **stack_a)
 {
@@ -51,7 +143,7 @@ t_list	*find_target(t_list *node, t_list **stack, int closest_bigger)
 
 t_list	*get_to_sort_operations(t_list **stack_a, t_list **stack_b)
 {
-	t_list	*tmp;
+	t_list 	*tmp;
 	t_list	*operations;
 
 	operations = NULL;
@@ -61,16 +153,23 @@ t_list	*get_to_sort_operations(t_list **stack_a, t_list **stack_b)
 	{
 		operations = ft_lstnew("sa\n");
 		apply_operations(stack_a, stack_b, operations);
+		return (operations);
 	}
 	else if (ft_lstsize(*stack_a) == 3)
 	{
 		tmp = sort_3_numbers(stack_a);
 		ft_lstadd_back(&operations, tmp);
 		apply_operations(stack_a, stack_b, tmp);
+		return (operations);
 	}
 	else if (ft_lstsize(*stack_a) == 4)
+	{
 		sort_4_numbers(stack_a, stack_b, &operations);
+		return (operations);
+	}
 	else
+	{
 		turk_sort_numbers(stack_a, stack_b, &operations);
-	return (operations);
+		return (operations);
+	}
 }
